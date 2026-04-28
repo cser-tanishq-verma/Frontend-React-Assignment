d709dd2b1c4fda5c3991d20329b1cac9b45225a9
cser-tanishq-verma <tanishqv29@icloud.com>
2026-04-28 10:51:22 +0530

    Delete 27-04-2026/reactx-app2 directory

--- 27-04-2026/reactx-app2/src/FunctionBased2.jsx
+++
@@ -1,4 +0,0 @@
-let arrowfunction=()=>{
-    return <h3>this is arrow function</h3>
-}
-export default arrowfunction
--- 27-04-2026/reactx-app2/src/FunctionBased3.jsx
+++
@@ -1,3 +0,0 @@
-export default ()=>{
-    return <h3>this is name function</h3>
-}
